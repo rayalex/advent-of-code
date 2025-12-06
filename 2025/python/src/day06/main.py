d9ba773f71ece5cc8529665dbb6585260a244931
alex <stingermssl@gmail.com>
2025-12-06 18:03:18 +0100

    Day 06

---
+++ 2025/python/src/day06/main.py
@@ -0,0 +1,64 @@
+from utils.io import read_lines_here
+from utils.matrix import Matrix
+from math import prod
+import re
+
+lines = read_lines_here("input.txt", __file__)
+
+# create a matrix by splitting the strings on space(s)
+data = Matrix.from_lines_col(lines, lambda x: x.strip().split())
+
+def part1(data_grid: Matrix) -> int:
+  """
+  Does column wise sum/product where:
+  Rows 0-N are the numbers
+  Row N is the operation (+ or *)
+
+  Total all the sub operations and return the result which is our solution to the puzzle.
+  """
+  total = 0
+
+  for col in range(data_grid.cols):
+    op = data_grid.get(data_grid.rows - 1, col)
+    sublist = []
+    for row in range(data_grid.rows - 1):
+      sublist.append(int(data_grid.get(row, col)))
+
+    subtotal = sum(sublist) if op == '+' else prod(sublist)
+    total += subtotal
+
+  return total
+
+def part2() -> int:
+  """
+  Treat the entire vertical column as a matrix of characters
+  Transpose it and convert rows to ints
+  Do the operation on those
+
+  We need to re-parse though, since the operation is aligned on the boundary
+  and not all numbers are aligned the same.
+  """
+  # re-read the file into matrix, but as characters
+  data_grid = Matrix.from_lines_col(read_lines_here("input.txt", __file__), lambda x: [char for char in x])
+
+  total = 0
+  ops = "".join(data_grid.get_row(data_grid.rows - 1)) # temp back to string
+
+  # use our regex matches as boundaries
+  for m in re.finditer(r"[\+|\*]\s+", ops):
+    # start, end correspond to columns where our math block is
+    start, end = m.start(), m.end()
+    op = m.group()[0]
+
+    math_block = data_grid \
+      .submatrix(0, data_grid.rows - 1, start, end) \
+      .transpose() # given we have col iter now this is not needed
+    
+    # iterate rows, make each row a number
+    numbers = [int(s) for row in math_block.rows_iter() if (s := "".join(row).strip())]
+    total += sum(numbers) if op == '+' else prod(numbers)
+
+  return total
+
+print("Part 1:", part1(data))
+print("Part 2:", part2())
--- 2025/python/src/utils/matrix.py
+++ 2025/python/src/utils/matrix.py
@@ -12,9 +12,25 @@ class Matrix(Generic[T]):
     def from_lines(lines: List[str], transform: Callable[[str], T]) -> 'Matrix[T]':
         data = [[transform(char) for char in line] for line in lines]
         return Matrix(data)
+    
+    @staticmethod
+    def from_lines_col(lines: List[str], col_transform: Callable[[str], List[T]]) -> 'Matrix[T]':
+        data = [col_transform(line) for line in lines]
+        return Matrix(data)
 
     def get(self, row: int, col: int) -> T:
         return self.data[row][col]
+    
+    def get_row(self, row: int) -> List[T]:
+        return self.data[row]
+    
+    def rows_iter(self):
+        for row in self.data:
+            yield row
+
+    def cols_iter(self):
+        for col in range(self.cols):
+            yield [self.data[row][col] for row in range(self.rows)]
 
     def set(self, row: int, col: int, value: T) -> None:
         self.data[row][col] = value
@@ -23,5 +39,9 @@ class Matrix(Generic[T]):
         transposed_data = [[self.data[j][i] for j in range(self.rows)] for i in range(self.cols)]
         return Matrix(transposed_data)
     
+    def submatrix(self, row_start: int, row_end: int, col_start: int, col_end: int) -> 'Matrix[T]':
+        sub_data = [row[col_start:col_end] for row in self.data[row_start:row_end]]
+        return Matrix(sub_data)
+    
     def __repr__(self) -> str:
-        return '\n'.join([''.join([str(item) for item in row]) for row in self.data])
+        return '\n'.join(['|'.join([str(item) for item in row]) for row in self.data])
